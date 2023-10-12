c1f4276c8a70460fcb368b2d829a2f49d5ee6a59
soufiane <soufian9mkns@gmail.com>
2023-10-12 13:58:43 -0700

    3- common

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,3 +1,15 @@
-#!/bin/usr/python3
+#!/usr/bin/python3
 def common_elements(set_1, set_2):
-    return ([x for x in set_1 if x in set_2])
+    """
+    find the common elements between 2 set arguments
+    set_1 == first set
+    set_2 == second set
+    """
+    return set_1.intersection(set_2)
+
+
+if __name__ == '__main__':
+    set_1 = {"Python", "C", "Javascript"}
+    set_2 = {"Bash", "C", "Ruby", "Perl"}
+    c_set = common_elements(set_1, set_2)
+    print(sorted(list(c_set)))
