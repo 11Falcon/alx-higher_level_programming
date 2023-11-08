4d86811f6bee4124e64a09c7c33c1f4823f29b95
soufiane <soufian9mkns@gmail.com>
2023-11-08 02:32:43 -0800

    pycode style

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
+
+
 """
     Addition function for test
     0-addinteger
     takes two values, both of them should be int or float
 """
+
+
 def add_integer(a, b=98):
     """
     Addition Function with test conditions
