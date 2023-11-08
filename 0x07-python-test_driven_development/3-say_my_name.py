c904acc244aac10e54f4b4a8b5b8edce40c2105b
soufiane <soufian9mkns@gmail.com>
2023-11-08 03:04:21 -0800

    say_my name funtion

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-python function 
+python function
 returns: My name is <first name> <last name>
 takes as input: first_name and last_name that must be strings
 """
@@ -8,11 +8,11 @@ takes as input: first_name and last_name that must be strings
 
 def say_my_name(first_name, last_name=""):
     """
-    say my name function 
+    say my name function
     """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     elif not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
     else:
-        print("My name is " +first_name + " " + last_name)
+        print("My name is " + first_name + " " + last_name)
