06bfdf06a75a9572fb91adcfb8e66e85ec003357
soufiane <soufian9mkns@gmail.com>
2023-11-08 02:15:40 -0800

    add integer funtion

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -4,10 +4,9 @@
 """
 def add_integer(a, b=98):
     """ Addition Function with test conditions"""
-    if type(a) not in [int, float]:
+    if not isinstance(a,(int, float)):
         raise TypeError("a must be an integer")
-    if type(b) not in [int, float]:
+    elif not isinstance(b,(int, float)):
         raise TypeError("b must be an integer")
-    a = int(a)
-    b = int(b)
-    return (a + b)
+    else:
+        return (int(a) + int(b))
