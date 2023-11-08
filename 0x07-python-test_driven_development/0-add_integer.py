4c75b9a49e83f0c993ef6b8af449f9cf6e1f4904
soufiane <soufian9mkns@gmail.com>
2023-11-08 02:20:16 -0800

    doc

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 """
     Addition function for test
+    0-addinteger
+    takes two values, both of them should be int or float
 """
 def add_integer(a, b=98):
-    """ Addition Function with test conditions"""
+    """
+    Addition Function with test conditions
+    """
     if not isinstance(a,(int, float)):
         raise TypeError("a must be an integer")
     elif not isinstance(b,(int, float)):
