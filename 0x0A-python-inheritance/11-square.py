2345cc9a1f4a9616e9f9e362d0dedb3eb49b66b2
soufiane <soufian9mkns@gmail.com>
2023-11-11 09:33:55 -0800

    square

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+ Square that inherits from rectangle
+"""
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangel):
+    """ inherits from Rectangle"""
+    def __init__(self, size):
+        super().integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        """str function that returns the next thing"""
+        return str("[Square] {}/{}".format(self.__size, self.__size))
+    def area(self):
+        """ return the area of the square"""
+        return self.__size ** 2
