6a80ca4ad1b0c2d1917e45a3f511bf35c9e41f47
Falcon <soufian9mkns@gmail.com>
2024-02-14 12:43:54 -0800

    create and filling table'

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,7 +1,6 @@
 -- creates a table second_table in database and add multiples rows
 CREATE TABLE IF NOT EXISTS second_table(id int, name VARCHAR(256), score int);
-INSERT INTO second_table(id, name, score) VALUES
-(1, "John", 10),
-(2, "Jane Smith", 30),
-(3, "Bob Johnson", 22),
-(4, "Alice Williams", 28);
+INSERT INTO second_table(id, name, score) VALUES (1, "John", 10);
+INSERT INTO second_table (id, name, score) VALUES (2, "Jane Smith", 30);
+INSERT INTO second_table (id, name, score) VALUES (3, "Bob Johnson", 22);
+INSERT INTO second_table (id, name, score) VALUES (4, "Alice Williams", 28);
