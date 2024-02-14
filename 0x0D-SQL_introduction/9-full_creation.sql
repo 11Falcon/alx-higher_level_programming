05cab0cb44b02df8c740504903302691eadfbc68
Falcon <soufian9mkns@gmail.com>
2024-02-14 10:06:22 -0800

    create none empty table

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,11 @@
+-- creates a table second_table in database and add multiples rows
+CREATE TABLE IF NOT EXISTS second_table(
+	id int,
+	name VARCHAR(256),
+	score int
+);
+INSERT INTO second_table(id, name, score) VALUES
+(1, "John", 10),
+(2, "Jane Smith", 30),
+(3, "Bob Johnson", 22),
+(4, "Alice Williams", 28);
