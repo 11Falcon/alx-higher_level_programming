ebcf8cdba1cf943961dce7ec25913e15ac3d1868
Falcon <soufian9mkns@gmail.com>
2024-02-15 07:21:22 -0800

    States table

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,4 @@
+-- creating the database hbtn_0d_usa and table tates
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE states( id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(256) NOT NULL);
