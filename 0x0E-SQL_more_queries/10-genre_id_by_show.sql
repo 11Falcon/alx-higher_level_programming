ae16b72b0d2236b288b74be0448cbc0acb7c2c7c
Falcon <soufian9mkns@gmail.com>
2024-02-15 13:28:46 -0800

    inner join

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,2 +1,2 @@
 -- listint all shows contained in hbtn_0d_tvshows
-SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genre.show_id ORDER BY tv_shows.title, tv_show_genres.genre_id;
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows INNER JOIN tv_shows_genres ON tv_shows.id = tv_shows_genre.show_id ORDER BY tv_shows.title, tv_show_genres.genre_id;
