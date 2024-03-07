6f7991985eaaf8066bb258578e14dab5e5d5ff08
Falcon <soufian9mkns@gmail.com>
2024-03-07 10:20:34 -0800

    append a string to a text file

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -5,5 +5,5 @@
 def append_write(filename="", text=""):
     """append a string to a text file"""
     with open(filename, "a", encoding="utf-8") as File:
-        File.append(text)
+        File.write(text)
         return len(text)
