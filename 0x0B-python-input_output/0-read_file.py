2c4723fa9c6ba4ab7584d9c80fb84eb7b9961a18
Falcon <soufian9mkns@gmail.com>
2024-03-07 07:42:32 -0800

    readline

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """Read file in utf-8 encoding"""
+
+
 def read_file(filename=""):
     """read the filename file"""
-    with open(filename, "r", encoding = "utf-8") as f:
+    with open(filename, "r", encoding="utf-8") as f:
         print(f.read(), end="")
