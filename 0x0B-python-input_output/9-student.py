c6e4d30f2f983bf9e2b32e22a62829452dc48e96
Falcon <soufian9mkns@gmail.com>
2024-03-09 00:50:02 -0800

    student class

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""class student"""
+
+
+class Student:
+    """a student instance containing his info"""
+    def __init__(self, first_name, last_name, age):
+        """initialise attributes"""
+        self.first_name = first_name
+        self.lat_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """retreive a dic"""
+        return (vars(self))
