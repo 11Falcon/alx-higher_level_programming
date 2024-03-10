80338abace5d70cceff6344534ef25cb711cb4cd
Falcon <soufian9mkns@gmail.com>
2024-03-10 01:34:28 -0800

    test

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,20 +1,18 @@
-#!/usr/bin/python3
-
-
+!/usr/bin/python3
 """
-    Addition function for test
-    0-addinteger
-    takes two values, both of them should be int or float
+This is the "0-add_integer" module.
+The 0-add_integer module supplies one function, add_integer(a, b).
 """
 
 
-def add_integer(a, b=98):
-    """
-    Addition Function with test conditions
-    """
-    if not isinstance(a, (int, float)):
+def add_integer(a, b):
+    """Return the addition of two numbers."""
+    if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
-    elif not isinstance(b, (int, float)):
+    if type(b) is not int and type(b) is not float:
         raise TypeError("b must be an integer")
-    else:
-        return (int(a) + int(b))
+    if type(a) is float:
+        a = int(a)
+    if type(b) is float:
+        b = int(b)
+    return a + b
--- 0x07-python-test_driven_development/0-main.py
+++ 0x07-python-test_driven_development/0-main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 add_integer = __import__('0-add_integer').add_integer
 
-print(add_integer(5, None))
+print(add_integer(float('inf'), None))
 try:
     print(add_integer(None, None))
 except Exception as e:
