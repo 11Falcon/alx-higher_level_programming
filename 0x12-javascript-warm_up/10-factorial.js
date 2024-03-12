1cf8beee594363e9d15b3f9c3b229b44ceaede26
soufiane <soufian9mkns@gmail.com>
2024-03-12 08:11:59 -0700

    10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,12 +1,15 @@
 #!/usr/bin/node
 const n = parseInt(process.argv[2]);
-let result = 1;
-if (!isNaN(n)) {
-  let i = 1;
-  for (i; i <= n; i++) {
-    result *= i;
+function factorial (n) {
+  let result = 1;
+  if (!isNaN(n)) {
+    let i = 1;
+    for (i; i <= n; i++) {
+      result *= i;
+    }
+  } else {
+    result = 1;
   }
-} else {
-  result = 1;
+  console.log(result);
 }
-console.log(result);
+factorial(n);
