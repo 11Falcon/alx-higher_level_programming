80f249ffc441a45a3fadc576e740bb8e1e5bd890
soufiane <soufian9mkns@gmail.com>
2024-03-12 05:43:09 -0700

    2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,10 @@
 #!/usr/bin/node
-const { argv } = require('node:process');
-if (argv[2] === undefined) {
+const argsCount = process.argv.length;
+
+if (argsCount === 2) {
   console.log('No argument');
+} else if (argsCount === 3) {
+  console.log('Argument found');
 } else {
   console.log('Arguments found');
 }
