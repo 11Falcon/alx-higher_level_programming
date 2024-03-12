3cc0f87ad5162aec882ca19db19ddbe9b2f5a379
soufiane <soufian9mkns@gmail.com>
2024-03-12 08:06:28 -0700

    10-factorial.js

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const n = parseInt(process.argv[2]);
+let result = 1;
+if (!isNaN(n)) {
+  let i = 1;
+  for (i; i < n; i++) {
+    result *= i;
+  }
+} else {
+  result = 1;
+}
+console.log(result);
