64101f24e4d3754996d8d35f51e150352689dd36
Falcon <soufian9mkns@gmail.com>
2024-03-18 16:10:47 -0700

    7-model_state_fetch_all.py

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
-"""script to list all state objects using sqlalchemy
 """
-from model_state import Base, State
+sqlalchemy to list all state objects in given database
+"""
+if __name__ == "__main__":
+    import sys
+    from model_state import Base, State
+    from sqlalchemy.orm import sessionmaker
+    from sqlalchemy import create_engine
 
-from sqlalchemy.orm import sessionmaker
-
-from sqlalchemy import (create_engine)
-
-import sys
-
-
-if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database_name = sys.argv[3]
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
-    # create custom session object class from database engine
-    Session = sessionmaker(bind=engine)
-    # create instance of new custom session class
+                           .format(username, password, database_name))
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker()
+    Session.configure(bind=engine)
     session = Session()
-    for state in session.query(State).order_by(State.id):
-        print('{}: {}'.format(state.id, state.name))
+    for instance in session.query(State).order_by(State.id):
+        print("{}: {}".format(instance.id, instance.name))
