72b6ebcac285e9d221a044fa37ee04d864f847bd
Falcon <soufian9mkns@gmail.com>
2024-03-18 15:57:05 -0700

    5-filter_cities.py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,39 +1,26 @@
 #!/usr/bin/python3
-"""script for use in getting all states from sql db
 """
-import MySQLdb
-import sys
+Takes in the name of a state as an argument
+and lists all cities of that state, using the database
+"""
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
 
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database_name = sys.argv[3]
+    state_name = sys.argv[4]
 
-if __name__ == '__main__':
-    args = sys.argv
-    if len(args) < 5:
-        print("Usage: {} username password db_name state_name".format(args[0]))
-        exit(1)
-
-    # connect to database and set up user input variables
-    username = args[1]
-    password = args[2]
-    data = args[3]
-    statename = args[4]
-    db = MySQLdb.connect(host='localhost', user=username,
-                         passwd=password, db=data,
-                         port=3306)
+    db = MySQLdb.connect(host='localhost', port=3306, user=username,
+                         passwd=password, db=database_name)
     cur = db.cursor()
-    # execute sql join statement to gather states and cities
-    num_rows = cur.execute('''
-        SELECT cities.id, cities.name, states.name
-        FROM cities INNER JOIN states
-        ON cities.state_id=states.id
-        ORDER BY cities.id ASC
-        ''')
-    rows = cur.fetchall()
-    # get cities from all rows matching state name
-    cities = [row[1] for row in rows if statename == row[2]]
-    num_cities = len(cities)
-    # print cities out using custom ends to format output
-    for i, city in enumerate(cities):
-        if i == num_cities - 1:
-            print(city)
-        else:
-            print(city, end=", ")
+
+    cur.execute("SELECT cities.name FROM cities JOIN states ON cities.state_id\
+                = states.id WHERE states.name LIKE BINARY %s ORDER BY\
+                cities.id;", (sys.argv[4], ))
+    result = cur.fetchall()
+    line = []
+    for row in result:
+        line.append(row[0])
+    print(", ".join(line))
